d4abb2ebb1d65b82e71f6d33877007ca21f6f6a1
SArthak3661 <guptasarthak451@gmail.com>
2019-08-18 23:02:17 +0530

    Intial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const socket = require('socket.io');
+
+const app = express();
+
+app.get('/', (req,res,next) => {
+    res.sendFile(path.join(__dirname, 'public', 'Desktop', 'index.html'));
+});
+
+app.get('/mobile', (req,res,next) => {
+    res.sendFile(path.join(__dirname, 'public', 'Mobile', 'mobile.html'));
+});
+
+let server = app.listen(8080);
+const io = socket(server); 
+
+io.on('connection', function(socket){
+    socket.on('orientation',function(e){
+        console.log(e);
+    });
+})
+
+
